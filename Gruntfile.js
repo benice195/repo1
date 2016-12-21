6bebd66e53bbf8222548eab63c01557aca461a81
Wojciech Szczyrba <benice195@gmail.com>
2016-12-21 22:23:31 +0100

    Zmiana_w_imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,9 @@ module.exports = function(grunt) {
         dynamic: {
             files: [{
                 expand: true,
-                cwd: 'images/',
+                cwd: 'index.html',
                 src: ['**/*.{png,jpg,gif}'],
-                dest: 'effectpr1new/'
+                dest: 'index.html'
             }]
         }
     },
